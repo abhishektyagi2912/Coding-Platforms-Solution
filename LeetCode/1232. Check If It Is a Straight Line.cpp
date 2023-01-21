352563579c70dfb759ebd276a19aaaa08ec00888
Abhishek Tyagi <abhishektyagi2912@gmail.com>
2023-01-21 21:49:05 +0530

    Create 1232. Check If It Is a Straight Line.cpp

---
+++ LeetCode/1232. Check If It Is a Straight Line.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& c) {
+        for(int i=2;i<c.size();i++)
+            if(((c[0][0]*(c[1][1]-c[i][1])) + (c[1][0]*(c[i][1]-c[0][1])) + (c[i][0]*(c[0][1]-c[1][1]))) != 0) return false;
+        return true;
+    }
+};
