cce2b2f6d7c0d9880263f38e4e72b1d18618f9c6
Abhishek Tyagi <abhishektyagi2912@gmail.com>
2023-05-05 11:33:35 +0530

    Create 257. Binary Tree Paths.cpp

---
+++ LeetCode/257. Binary Tree Paths.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    void path(TreeNode* root, vector<string>&ans,string value){
+        if(root->right==NULL && root->left== NULL){
+            ans.push_back(value + to_string(root->val));
+            return;
+        }
+        if(root->left) path(root->left, ans, value+to_string(root->val)+"->");
+        if(root->right) path(root->right, ans, value+to_string(root->val)+"->");
+    }
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string>ans;
+        path(root,ans,"");
+        return ans;
+    }
+};
