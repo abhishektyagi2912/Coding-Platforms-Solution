ffe129b2fd5e55f1677e519c3923cefcfc69aeaf
Abhishek Tyagi <abhishektyagi2912@gmail.com>
2023-07-01 23:30:50 +0530

    Create 11. Container With Most Water.cpp

---
+++ LeetCode/11. Container With Most Water.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l =0;
+        int r= height.size()-1;
+        int ans=0;
+        while(l<r){
+            ans = max(min(height[l],height[r])*(r-l),ans);
+            if(height[l]<height[r]){
+                l++;
+            }
+            else r--;
+        }
+        return ans;
+    }
+};
