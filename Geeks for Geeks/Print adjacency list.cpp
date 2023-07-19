6f5643c1c394b498d55fe0f0aa7d50ba96c34735
Abhishek Tyagi <abhishektyagi2912@gmail.com>
2023-07-19 22:27:25 +0530

    Create Print adjacency list.cpp

---
+++ Geeks for Geeks/Print adjacency list.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    // Function to return the adjacency list for each vertex.
+    vector<vector<int>> printGraph(int V, vector<int> adj[]) {
+        // Code here
+        vector<vector<int>> ans;
+        
+        for(int i=0;i<V;i++){
+            vector<int> temp;
+            temp.push_back(i);
+            for(auto x: adj[i]){
+                temp.push_back(x);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
